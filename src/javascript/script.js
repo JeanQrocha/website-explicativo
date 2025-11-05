234ee55c65ee4e4ac727ff04124f8ffc81c35f66
Jean Rocha <jean.qrocha96@gmail.com>
2025-11-05 19:15:14 -0300

    atualizado javascript

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,24 +1,24 @@
-// ================
+
 function mostrarAlerta() {
   document.getElementById("alertResult").innerText = "Exibindo alerta...";
   alert("Olá, mundo!");
   document.getElementById("alertResult").innerText = "Alerta exibido com sucesso!";
 }
 
-// ================
+
 function mostrarConsole() {
   console.log("Mensagem no console!");
   document.getElementById("consoleResult").innerText = "Mensagem registrada no console.";
 }
 
-// ===============
+
 function mostrarVariaveis() {
-  let nome = "Jean";
-  const idade = 29;
-  document.getElementById("variaveisResult").innerText = `Nome: ${nome}, Idade: ${idade}`;
+  let variavelLet = "let é uma variavel que pode ser reatribuido";
+  const variavelConst = "const é uma variavel que nao pode ser reatribuido";
+  document.getElementById("variaveisResult").innerText = `Let: ${variavelLet},  Const: ${variavelConst}`;
 }
 
-// ================
+
 function mostrarTipos() {
   let texto = "Olá";
   let numero = 10;
@@ -27,14 +27,14 @@ function mostrarTipos() {
     `Texto: "${texto}" (${typeof texto}), Número: ${numero} (${typeof numero}), Booleano: ${booleano} (${typeof booleano})`;
 }
 
-// ================
+
 function verificarIdade() {
   let idade = 18;
   let resultado = idade >= 18 ? "Maior de idade" : "Menor de idade";
   document.getElementById("condicionalResult").innerText = resultado;
 }
 
-// ================
+
 function mostrarLoop() {
   let resultado = "";
   for (let i = 1; i <= 5; i++) {
